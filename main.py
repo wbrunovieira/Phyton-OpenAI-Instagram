10a8e4cfa8cf7c369b30b99805f417d0b1314582
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-04 20:35:20 -0300

    added chagpt conection to make a instagram content done

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def openai_whisper_trascrever(caminho_audio, nome_arquivo,modelo_whisper, openai
         model=modelo_whisper,
         file =audio
         
+    
+        
  ) 
     transcricao = resposta.text
     
@@ -20,8 +22,45 @@ def openai_whisper_trascrever(caminho_audio, nome_arquivo,modelo_whisper, openai
         
     return transcricao
     
-    print("Transcricão salva com sucesso!")
 
+
+def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai):
+    print("Resumindo com o gpt para um post do instagram ...")
+
+    prompt_sistema = """
+    Assuma que você é um digital influencer digital e que está construíndo conteúdos das áreas de tecnologia em uma plataforma de áudio (podcast).
+
+    Os textos produzidos devem levar em consideração uma peresona que consumirá os conteúdos gerados. Leve em consideração:
+
+    - Seus seguidores são pessoas super conectadas da área de tecnologia, que amam consumir conteúdos relacionados aos principais temas da área de computação.
+    - Você deve utilizar o gênero neutro na construção do seu texto
+    - Os textos serão utilizados para convidar pessoas do instagram para consumirem seu conteúdo de áudio
+    - O texto deve ser escrito em português do Brasil.
+
+    """
+    prompt_usuario = ". \nReescreva a transcrição acima para que possa ser postado como uma legenda do Instagram. Ela deve resumir o texto para chamada na rede social. Inclua hashtags"
+
+    resposta = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system",
+             
+             "content": prompt_sistema
+             },
+            {
+                "role": "user",
+                "content": transcricao_completa + prompt_usuario
+            }
+        ],
+        temperature=0.6
+    )
+    
+    resumo_instagram = resposta["choices"][0]["message"]["content"]
+    
+    with open(f"resumo_instagram/{nome_arquivo}.txt", "w", encoding="utf-8") as arquivo_texto:
+        arquivo_texto.write(resumo_instagram)
+        
+    return resumo_instagram
 def main():
     load_dotenv()
     
@@ -36,6 +75,7 @@ def main():
     modelo_whisper = "whisper-1"
     
     transcricao_completa = openai_whisper_trascrever(caminho_audio, nome_arquivo, modelo_whisper, openai)
+    resumo_instagram = openai_gpt_resumir_texto( transcricao_completa, nome_arquivo, openai)
 
 if __name__ == "__main__":
     
