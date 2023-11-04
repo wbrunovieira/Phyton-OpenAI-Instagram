0602cf078d829c8a77d0e55fbe20294afff74075
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-04 19:48:03 -0300

    start openai conctions an importsf

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import os
 
 def main():
     load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    caminho_audio = "podcasts/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.mp3"
+    nome_arquivo = "ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146"
+
+    url_podcast = "https://www.youtube.com/watch?v=YZ6YZAvDHXA"
+    
+    api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = api_key
 
 if __name__ == "__main__":
     
-main()
+    main()
    
 
