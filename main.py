082d3a0e708c416d071fb6262c290603ea52f3fc
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-08 21:44:14 -0300

    longs mp3, cut to send to api

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import requests
 from pydub import AudioSegment
+
+
 def openai_whisper_trascrever(caminho_audio, nome_arquivo,modelo_whisper, openai):
     print("Transcrevendo o audio...")
     
@@ -193,6 +195,51 @@ def transcricao_completa_nvidia(caminho_audio, nome_arquivo, modelo_whisper, ope
         arquivo_texto.write(transcricao)
         
     return transcricao 
+
+def ferramenta_transcrever_audio_em_partes(caminho_audio_podcast, nome_arquivo):
+    print("Iniciando corte .. ")
+    audio = AudioSegment.from_mp3(caminho_audio_podcast)
+
+    dez_minutos = 10 * 60 * 1000
+    
+    contador_pedaco = 1
+    arquivos_exportados = []
+
+    while len(audio) > 0:
+        pedaco = audio[:dez_minutos]
+        nome_pedaco_audio = f"{nome_arquivo}_parte_{contador_pedaco}.mp3"
+        pedaco.export(nome_pedaco_audio, format="mp3")
+        arquivos_exportados.append(nome_pedaco_audio)
+        audio = audio[dez_minutos:]
+        contador_pedaco += 1
+
+    return arquivos_exportados
+def openai_whisper_trascrever_em_partes(caminho_audio, nome_arquivo,modelo_whisper, openai):
+    print("Transcrevendo o audio...")
+    lista_arquivos_de_audio = ferramenta_transcrever_audio_em_partes(caminho_audio, nome_arquivo)
+    lista_pedacos_de_audio = []
+    
+    for um_pedacao_de_audio in lista_arquivos_de_audio:
+    
+        audio = open(um_pedacao_de_audio, "rb")
+        
+        resposta = openai.Audio.transcribe(
+            api_key=openai.api_key,
+            model=modelo_whisper,
+            file =audio
+            
+        
+            
+    ) 
+        transcricao = resposta.text
+        lista_pedacos_de_audio.append(transcricao)
+        
+    transcricao = "".join(lista_pedacos_de_audio)
+    
+    with open(f"transcricoes/{nome_arquivo}.txt", "w", encoding="utf-8") as arquivo_texto:
+        arquivo_texto.write(transcricao)
+        
+    return transcricao
 def main():
     load_dotenv()
     
@@ -201,7 +248,7 @@ def main():
     caminho_audio = "podcasts/tomp3.cc -Como a Inteligência Artificial IA irá revolucionar empresas e negócios ft Aster  AI 360 02.mp3"
     nome_arquivo = "COmo a Inteligenca Aurtificial IA irá revolucionar empresas e negócios ft Aster  AI 360 02"
 
-    url_podcast = "https://www.youtube.com/watch?v=YZ6YZAvDHXA"
+    url_podcast = ""
     
     resolucao = "1024x1024"
     qtd_imagens = 4
@@ -228,7 +275,7 @@ def main():
     
     #ferramenta_download_imagem(nome_arquivo, imagem_gerada,qtd_imagens)
     
-    transcricao_completa_nvidia = openai_whisper_trascrever(caminho_audio, nome_arquivo, modelo_whisper, openai)
+    transcricao_completa_nvidia = openai_whisper_trascrever_em_partes(caminho_audio, nome_arquivo, modelo_whisper, openai)
     
 if __name__ == "__main__":
     
