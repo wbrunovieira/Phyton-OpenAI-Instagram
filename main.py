67b73d2d31427ca2b7563bdf8f281d2e5c4f5b79
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-09 22:17:50 -0300

    refactored for new openai version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from openai import OpenAI
 import openai
+
 from dotenv import load_dotenv
 import os
 import requests
@@ -27,38 +29,35 @@ def openai_whisper_trascrever(caminho_audio, nome_arquivo,modelo_whisper, openai
     
 
 
-def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai):
+def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo):
     print("Resumindo com o gpt para um post do instagram ...")
 
     prompt_sistema = """
     Assuma que você é um digital influencer digital e que está construíndo conteúdos das áreas de tecnologia em uma plataforma de áudio (podcast).
 
-    Os textos produzidos devem levar em consideração uma peresona que consumirá os conteúdos gerados. Leve em consideração:
+    Os textos produzidos devem levar em consideração uma persona que consumirá os conteúdos gerados. Leve em consideração:
 
     - Seus seguidores são pessoas super conectadas da área de tecnologia, que amam consumir conteúdos relacionados aos principais temas da área de computação.
     - Você deve utilizar o gênero neutro na construção do seu texto
     - Os textos serão utilizados para convidar pessoas do instagram para consumirem seu conteúdo de áudio
     - O texto deve ser escrito em português do Brasil.
-
     """
-    prompt_usuario = ". \nReescreva a transcrição acima para que possa ser postado como uma legenda do Instagram. Ela deve resumir o texto para chamada na rede social. Inclua hashtags"
+    
+    prompt_usuario = f"{prompt_sistema} {transcricao_completa} \nReescreva a transcrição acima para que possa ser postado como uma legenda do Instagram. Ela deve resumir o texto para chamada na rede social. Inclua hashtags"
+    
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    resposta = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system",
-             
-             "content": prompt_sistema
-             },
-            {
-                "role": "user",
-                "content": transcricao_completa + prompt_usuario
-            }
-        ],
-        temperature=0.6
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+         messages=[
+        {
+            "role": "user",
+            "content": prompt_usuario,
+        },
+    ],
     )
     
-    resumo_instagram = resposta["choices"][0]["message"]["content"]
+    resumo_instagram = response.choices[0].message.content
     
     with open(f"resumo_instagram/{nome_arquivo}.txt", "w", encoding="utf-8") as arquivo_texto:
         arquivo_texto.write(resumo_instagram)
@@ -89,23 +88,23 @@ def openai_gpt_criar_hashtag(resumo_instagram, nome_arquivo, openai):
     """
 
     prompt_usuario =f'Aqui está um resumo de um texto "{resumo_instagram}". Por favor, gere 5 hashtags que sejam relevantes para este texto e que possam ser publicadas no Instagram.  Por favor, faça isso em português do Brasil '
+    
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    resposta = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-             
-             "content": prompt_sistema
-             },
-            {
-                "role": "user",
-                "content":  prompt_usuario
-            }
-        ],
-        temperature=0.6
+         messages=[
+        {
+            "role": "user",
+            "content": prompt_usuario,
+        },
+    ],
     )
     
-    hashtags = resposta["choices"][0]["message"]["content"]
+    hashtags = response.choices[0].message.content
+
+    
+
     
     with open(f"hashtags/{nome_arquivo}.txt", "w", encoding="utf-8") as arquivo_texto:
         arquivo_texto.write(hashtags)
@@ -125,56 +124,83 @@ def openai_gpt_gerar_texto_imagem(resumo_instagram, nome_arquivo, openai):
 
     prompt_usuario =  f'Reescreva o texto a seguir, em uma frase, para que descrever o texto abaixo em um tweet: {resumo_instagram}'
     
-    resposta = openai.ChatCompletion.create(
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-             
-             "content": prompt_sistema
-             },
-            {
-                "role": "user",
-                "content":  prompt_usuario
-            }
-        ],
-        temperature=0.6
+         messages=[
+        {
+            "role": "user",
+            "content": prompt_usuario,
+        },
+    ],
     )
     
-    texto_para_imagem = resposta["choices"][0]["message"]["content"]
+    texto_para_imagem = response.choices[0].message.content
+   
     
     with open(f"texto_para_imagem/{nome_arquivo}.txt", "w", encoding="utf-8") as arquivo_texto:
         arquivo_texto.write(texto_para_imagem)
-    
+    texto_para_imagem = sanitize_filename(texto_para_imagem)
+    print("nome do arquivo depois de sanitizar",texto_para_imagem)
     return texto_para_imagem
+def sanitize_filename(filename):
+    # Substitui espaços por underscores
+    sanitized = filename.replace(" ", "_").replace("ç", "c").replace("ã", "a")
+    # Adicione aqui mais substituições se necessário, por exemplo:
+    # sanitized = sanitized.replace("ç", "c").replace("ã", "a")
+    return sanitized
 
 def openai_dalle_gerar_imagem(resolucao, resumo_para_imagem, nome_arquivo, openai, qtd_imagens = 1):
     print("Gerando a imagem com o DALL-E ...")
     
     prompt_user = f"Uma pintura ultra futurista, textless, 3d que retrate: {resumo_para_imagem}"
     
-    resposta = openai.Image.create(
-        prompt=prompt_user,
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = client.images.generate(
+        
+       prompt=prompt_user,
         n = qtd_imagens,
-        size = resolucao
+        size = resolucao,
+        quality="standard"
     )
     
-    return resposta["data"]
-
-def ferramenta_download_imagem(nome_arquivo, imagem_gerada,qtd_imagens = 1):
-  lista_nome_imagens = []
-  try:
-    for contador_imagens in range(0,qtd_imagens):
-        caminho = imagem_gerada[contador_imagens].url
-        imagem = requests.get(caminho)
-
-        with open(f"{nome_arquivo}_{contador_imagens}.png", "wb") as arquivo_imagem:
-            arquivo_imagem.write(imagem.content)
-
-        lista_nome_imagens.append(f"{nome_arquivo}_{contador_imagens}.png")
+    imagem_url = response.data[0].url
+    print("imagem_url",imagem_url)
+    
+    lista_nome_imagens = ferramenta_download_imagem(nome_arquivo, [imagem_url], qtd_imagens)
+    print("lista_nome_imagens", lista_nome_imagens)
     return lista_nome_imagens
-  except:
-    print("Ocorreu um erro!")
-    return  None
+def ferramenta_download_imagem(nome_arquivo, urls_imagens, qtd_imagens=1):
+    lista_nome_imagens = []
+    if not urls_imagens:  # Verifica se a lista de URLs está vazia
+        print("Nenhuma URL de imagem fornecida.")
+        return None
+
+    try:
+        for contador_imagens in range(qtd_imagens):
+            # Verifica se o índice está dentro do intervalo da lista
+            if contador_imagens >= len(urls_imagens):
+                print("O índice da lista de URLs de imagem está fora do alcance.")
+                break
+
+            caminho = urls_imagens[contador_imagens]
+            imagem = requests.get(caminho)
+
+            # Sanitiza o nome do arquivo
+            nome_arquivo_seguro = sanitize_filename(nome_arquivo)
+
+            nome_arquivo_completo = f"{nome_arquivo_seguro}_{contador_imagens}.png"
+            with open(nome_arquivo_completo, "wb") as arquivo_imagem:
+                arquivo_imagem.write(imagem.content)
+
+            lista_nome_imagens.append(nome_arquivo_completo)
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        return None
+
+    return lista_nome_imagens
 
 def transcricao_completa_nvidia(caminho_audio, nome_arquivo, modelo_whisper, openai):
     print("Transcrevendo o audio...")
@@ -253,29 +279,31 @@ def main():
     resolucao = "1024x1024"
     qtd_imagens = 4
     
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    # openai.api_key = os.getenv("OPENAI_API_KEY")
     
-    api_key = os.getenv("OPENAI_API_KEY")
-    openai.api_key = api_key
+    # api_key = os.getenv("OPENAI_API_KEY")
+    # openai.api_key = api_key
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     
     modelo_whisper = "whisper-1"
     
-    transcricao_completa = ferramenta_ler_arquivo("transcricoes/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt")  
-    resumo_instagram = ferramenta_ler_arquivo("transcricoes/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt") 
-    # transcricao_completa = openai_whisper_trascrever(caminho_audio, nome_arquivo, modelo_whisper, openai)
-    # resumo_instagram = openai_gpt_resumir_texto( transcricao_completa, nome_arquivo, openai)
+    #transcricao_completa = ferramenta_ler_arquivo("transcricoes/COmo a Inteligenca Aurtificial IA irá revolucionar empresas e negócios ft Aster  AI 360 02.txt")  
+    resumo_instagram = ferramenta_ler_arquivo("transcricoes/COmo a Inteligenca Aurtificial IA irá revolucionar empresas e negócios ft Aster  AI 360 02.txt") 
+    #transcricao_completa = openai_whisper_trascrever(caminho_audio, nome_arquivo, modelo_whisper, openai)
     
-    # hashtags = openai_gpt_criar_hashtag(resumo_instagram,nome_arquivo, openai)
-   # hashtags = ferramenta_ler_arquivo("hashtags/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt")
+    #resumo_instagram = openai_gpt_resumir_texto(str(transcricao_completa), nome_arquivo)
+    
+    #hashtags = openai_gpt_criar_hashtag(resumo_instagram,nome_arquivo, openai)
+    hashtags = ferramenta_ler_arquivo("hashtags/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt")
 
-    # resumo_imagem_instagram = openai_gpt_gerar_texto_imagem(resumo_instagram, nome_arquivo, openai)
-    #resumo_imagem_instagram = ferramenta_ler_arquivo("texto_para_imagem/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt")
+    #resumo_imagem_instagram = openai_gpt_gerar_texto_imagem(resumo_instagram, nome_arquivo, openai)
+    resumo_imagem_instagram = ferramenta_ler_arquivo("texto_para_imagem/ESSA_A_ PREVISÃO_ MAIS_ BIZARRA_PARA_O _FUTURO_ Os_Sócios_146.txt")
     
-    #imagem_gerada = openai_dalle_gerar_imagem(resolucao,resumo_imagem_instagram, nome_arquivo, openai, qtd_imagens)
+    imagem_gerada = openai_dalle_gerar_imagem(resolucao,resumo_imagem_instagram, nome_arquivo, openai, qtd_imagens)
     
-    #ferramenta_download_imagem(nome_arquivo, imagem_gerada,qtd_imagens)
+    ferramenta_download_imagem(nome_arquivo, imagem_gerada,qtd_imagens)
     
-    transcricao_completa_nvidia = openai_whisper_trascrever_em_partes(caminho_audio, nome_arquivo, modelo_whisper, openai)
+    #transcricao_completa_nvidia = openai_whisper_trascrever_em_partes(caminho_audio, nome_arquivo, modelo_whisper, openai)
     
 if __name__ == "__main__":
     
